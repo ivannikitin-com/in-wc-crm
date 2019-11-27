c1f5c8b1ee71103292f0d8d3e9fcbf4049fa508c
Ivan Nikitin <ivan.g.nikitin@gmail.com>
2019-11-27 10:45:45 +0300

    Добавлены элементы управления PickPoint и DataTable

--- extensions/PickPoint/PickPoint.php
+++ extensions/PickPoint/PickPoint.php
@@ -16,10 +16,8 @@ class PickPoint extends BaseAdminPage
     public function __construct()
     {
         parent::__construct();
-
+        add_action( 'admin_enqueue_scripts', array( $this, 'enqueueScripts' ) );
     }
-
-
 
     /**
      * Возвращает название расширения
@@ -38,6 +36,49 @@ class PickPoint extends BaseAdminPage
     {
         return __( 'PickPoint', IN_WC_CRM );
     }
+
+    /**
+     * Возвращает название пункта меню
+     * @return string
+     */
+    public function enqueueScripts()
+    {
+        // jQuery
+        wp_enqueue_script('jquery');
+        wp_enqueue_script('jquery-ui-autocomplete');
+        wp_enqueue_script('jquery-ui-datepicker');
+        wp_enqueue_style( 'jquery-ui-theme', '//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css' );
+
+        // DataTables
+        $dataTables = IN_WC_CRM . '-datatables';
+        wp_register_script( 
+            $dataTables, 
+            Plugin::get()->url . 'extensions/PickPoint/DataTables/datatables.min.js', 
+            array( 'jquery', 'jquery-ui-autocomplete', 'jquery-ui-datepicker' ),
+            Plugin::get()->version, 
+            true );
+        wp_enqueue_script( $dataTables ); 
+        wp_enqueue_style( $dataTables, Plugin::get()->url . 'extensions/PickPoint/DataTables/datatables.min.css' );         
+
+        // Скрипты расширения
+        $scriptID = IN_WC_CRM . '-pickpoint';
+        wp_register_script( 
+            $scriptID, 
+            Plugin::get()->url . 'extensions/PickPoint/js/pickpoint.js', 
+            array( 'jquery', 'jquery-ui-autocomplete', 'jquery-ui-datepicker', $dataTables ),
+            Plugin::get()->version, 
+            true );
+        wp_enqueue_script( $scriptID );    
+        
+        // Параметры для скриптов
+        $objectName = 'IN_WC_CRM_Pickpoint';
+        $data = array(
+            'orderStatuses' => wc_get_order_statuses()
+        );
+        wp_localize_script( $scriptID, $objectName, $data );
+
+    }
+
 
     /**
      * Отрисовывает содержимое страницы
--- extensions/PickPoint/views/order-list.php
+++ extensions/PickPoint/views/order-list.php
@@ -5,7 +5,71 @@
  */
 @include 'header.php';
 
+?>
+<section id="pickpointControls">
+  <span class="ui-widget">
+      <label for="statuses"><?php esc_html_e( 'Статусы заказов', IN_WC_CRM ); ?></label>
+      <input id="statuses" size="50">
+  </span>
 
+  <span>
+      <label for="dateFrom"><?php esc_html_e( 'Начальная дата', IN_WC_CRM ); ?></label>
+      <input id="dateFrom" type="text" class="datePickers">
+
+      <label for="dateTo"><?php esc_html_e( 'Конечная дата', IN_WC_CRM ); ?></label>
+      <input id="dateTo" type="text" class="datePickers">
+  </span>
+
+  <button><?php esc_html_e( 'Найти заказы', IN_WC_CRM ); ?></button>
+</section>
+
+<hr>
+
+<section id="pickpointDataTable">
+<style>
+  #orderTable tbody tr td { text-align: center }
+</style>
+<table id="orderTable" class="display" style="width:100%">
+        <thead>
+            <tr>
+                <th>&nbsp;</th>
+                <th>№ Заказа</th>
+                <th>ФИО</th>
+                <th>Сумма</th>
+                <th>Платеж</th>
+                <th>Доставка</th>
+                <th>Склад</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>*</td>
+                <td>42</td>
+                <td>Пупкин</td>
+                <td>6100</td>
+                <td>Наличные</td>
+                <td>PickPoint</td>
+                <td>15</td>
+            </tr>
+        </tbody>
+        <tfoot>
+            <tr>
+            <th>&nbsp;</th>
+                <th>№ Заказа</th>
+                <th>ФИО</th>
+                <th>Сумма</th>
+                <th>Платеж</th>
+                <th>Доставка</th>
+                <th>Склад</th>
+            </tr>
+        </tfoot>
+    </table>
+</section>
+
+
+
+
+<?php
 // Запрос списка заказов
 $query = new WC_Order_Query( array(
     'limit' => 10,
@@ -15,4 +79,4 @@ $query = new WC_Order_Query( array(
 ) );
 $orders = $query->get_orders();
 
-var_dump($orders);
+//var_dump($orders);
