9f3a0ca4a41ce5f724433a00e07624982c070ebd
Ivan Nikitin <ivan.g.nikitin@gmail.com>
2020-02-06 17:00:00 +0300

    Указана текущая версия плагина

--- in-wc-crm.php
+++ in-wc-crm.php
@@ -3,7 +3,7 @@
 * Plugin Name: IN WooCommerce CRM
 * Plugin URI: https://ivannikitin-com.github.io/in-wc-crm/
 * Description: Простая CRM система для WordPress и WooCommerce
-* Version: 1.0.20200130
+* Version: 1.3.20200206
 * Author: Иван Никитин и партнеры
 * Author URI: https://ivannikitin.com
 * License:     GPL3
