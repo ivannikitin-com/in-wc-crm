f9d80a2933665d5abf7d6113730bf5fa034b4a66
Ivan Nikitin <ivan.g.nikitin@gmail.com>
2020-10-29 18:32:49 +0300

    Добавлены новые поля для проверки

--- extensions/OrderTags/Condition.php
+++ extensions/OrderTags/Condition.php
@@ -27,8 +27,21 @@ class Condition
     {
         // Параметры проверки
         $this->params = array(
-            'order_items'           =>  __( 'Число товаров в заказе', IN_WC_CRM ),
-            'order_items_count'     =>  __( 'Число позиций в заказе', IN_WC_CRM ),
+            'customer_registration' =>  __( 'Пользователь зарегистрирован (Да/Нет)', IN_WC_CRM ),
+            'customer_name'         =>  __( 'Имя пользователя', IN_WC_CRM ),
+            'customer_email'        =>  __( 'E-mail пользователя', IN_WC_CRM ),
+            'customer_phone'        =>  __( 'Телефон пользователя', IN_WC_CRM ),
+            'billing_type'          =>  __( 'Способ оплаты', IN_WC_CRM ),
+            'shipping_type'         =>  __( 'Способ доставки', IN_WC_CRM ),
+            'billing_country'       =>  __( 'Страна оплаты', IN_WC_CRM ),
+            'billing_city'          =>  __( 'Город оплаты', IN_WC_CRM ),
+            'shipping_country'      =>  __( 'Страна доставки', IN_WC_CRM ),
+            'shipping_city'         =>  __( 'Город доставки', IN_WC_CRM ),
+            'order_total'           =>  __( 'Сумма заказа', IN_WC_CRM ),
+            'order_items'           =>  __( 'Число единиц товаров в заказе', IN_WC_CRM ),
+            'order_items_count'     =>  __( 'Число товарных позиций в заказе', IN_WC_CRM ),
+            'order_item_sku'        =>  __( 'Артикул товара', IN_WC_CRM ),
+            'order_item_title'      =>  __( 'Название товара', IN_WC_CRM ),
         );
 
         // Операции сравнения
@@ -77,7 +90,8 @@ class Condition
             $value =  $condition['value'];
 
             $result = false;
-            switch ($condition['equal']){
+            switch ($condition['equal'])
+            {
                 case 'eq':
                     $result = ($param == $value);
                 break;
@@ -103,18 +117,16 @@ class Condition
                 break;
 
                 case 're':
-                    // Возвращаем false в случае НЕ СООТВЕТСТВИЯ регулярному выражению
-                    $result = (bool) (preg_match('/' . $value . '/', $param ));                  
+                    $result = mb_ereg_match( $value, $param, 'iz' ); 
                 break;
 
                 case '!re':
-                    $result = (bool) ! (preg_match('/' . $value . '/', $param )); 
+                    $result = ! mb_ereg_match( $value, $param, 'iz' ); 
                 break;
-                
-                default:
-                    // Новые операции, возможно определенные фильром
-                    $result = apply_filters( 'inwccrm_ordertags_check', $order, $param, $condition, $value, false );
             }
+
+            // Новые операции, возможно определенные фильром
+            $result = apply_filters( 'inwccrm_ordertags_check', $result, $order, $param, $condition, $value );
 
             // Если результат false, дальше проверку можно не делать (ленивое вычисление)
             if ( ! $result ) return false;
@@ -134,6 +146,54 @@ class Condition
         $paramValue = '';
         switch($param)
         {
+            // ---------------------- Заказчик ----------------------
+            case 'customer_registration':
+                $user = get_user_by( 'email', $order->get_billing_email() );
+                $paramValue = ( isset($user->ID) ) ? __( 'Да', IN_WC_CRM ) : __( 'Нет', IN_WC_CRM );
+            break;
+
+            case 'customer_name':  
+                $paramValue = $order->get_formatted_billing_full_name();
+            break;
+
+            case 'customer_email':  
+                $paramValue = $order->get_billing_email();
+            break;
+
+            case 'customer_phone':  
+                $paramValue = $order->get_billing_phone();
+            break;
+
+            // ------------------------- Заказ -----------------------
+            case 'billing_type':  
+                $paramValue = $order->get_payment_method_title();
+            break;
+
+            case 'shipping_type':  
+                $paramValue = $order->get_shipping_method();
+            break;
+
+            case 'billing_country':  
+                $paramValue = $order->get_billing_country();
+            break;
+
+            case 'billing_city':  
+                $paramValue = $order->get_billing_city();
+            break;
+
+            case 'shipping_country':  
+                $paramValue = $order->get_shipping_country();
+            break;
+
+            case 'shipping_city':  
+                $paramValue = $order->get_shipping_city();
+            break;
+
+            case 'order_total':  
+                $paramValue = $order->calculate_totals();
+            break;            
+
+            // -------------------- Элементы заказа ------------------
             case 'order_items':  
                 $paramValue = $order->get_item_count();
             break;
@@ -141,8 +201,29 @@ class Condition
             case 'order_items_count':  
                 $paramValue = count( $order->get_items() );
             break;
+
+            case 'order_item_sku':
+                $skuArray = array();
+                foreach( $order->get_items() as $item_id => $item )
+                {
+                    $product = $item->get_product();
+                    $skuArray[] = $product->get_sku();
+                }
+                $paramValue = implode("\t", $skuArray);
+            break;
+            
+            case 'order_item_title':
+                $titleArray = array();
+                foreach( $order->get_items() as $item_id => $item )
+                {
+                    $titleArray[] = $item->get_name();
+                }
+                $paramValue = implode("\t", $titleArray);
+            break;           
+
         }
-        return apply_filters( 'inwccrm_ordertags_get_param', $order, $param, $paramValue );
+
+        return apply_filters( 'inwccrm_ordertags_get_param', $paramValue, $order, $param );
     }
 
 
