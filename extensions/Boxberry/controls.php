5ca89e6bc5861c2a213dff98d2592962ea154094
IrinaFedorova <irina.fed@outlook.com>
2025-09-02 15:27:52 +0300

    Изменено название кнопки на странице заказов: вместо Boxberry теперь Яндекс.

--- extensions/Boxberry/controls.php
+++ extensions/Boxberry/controls.php
@@ -1,1 +1,1 @@
-<button id="btnBoxberry"><i class="fas fa-retweet"></i>&nbsp;<?php esc_html_e( 'Boxberry', IN_WC_CRM ); ?></button>
+<button id="btnBoxberry"><i class="fas fa-retweet"></i>&nbsp;<?php esc_html_e( 'Яндекс', IN_WC_CRM ); ?></button>
